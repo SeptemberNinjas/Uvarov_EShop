14818907c28972b0277ff6b6afb97bc3d1d7361c
mistersavage <michael.um50@yandex.ru>
2024-10-07 13:47:54 +1000

    Задание 2. Задача минимальное и среднее значение

---
+++ minMaxFromArr/minMaxFromArr/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace minMaxFromArr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] numbers = { 5, 12, 2, 0, 20, 1500 };
+
+            int minValue = numbers[0];
+            int sum = 0;
+            int count = 0;
+
+            
+            foreach (int number in numbers)
+            {
+                if (number < minValue)
+                {
+                    minValue = number;
+                }
+
+                sum += number;
+                count++;
+            }
+
+            double average = (double)sum / count;
+
+            Console.WriteLine($"Минимальное значение: {minValue}");
+            Console.WriteLine($"Среднее значение: {average}");
+        }
+    }
+}
