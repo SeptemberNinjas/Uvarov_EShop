5a083e0ea8876cbf54d852dc2299913aa1d6a10b
mistersavage <michael.um50@yandex.ru>
2024-10-10 18:07:46 +1000

    Задание 6. Зарплата

---
+++ Task6-salaries/Task6-salaries/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task6_salaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
