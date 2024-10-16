b01e98c1ac5336e82419d936acff1801d8aeb5c1
mistersavage <michael.um50@yandex.ru>
2024-10-16 21:12:54 +1000

    изменил название проектов

--- Lesson1/4-minMaxFromArr/minMaxFromArr/obj/Debug/net8.0/minMaxFromArr.AssemblyInfo.cs
+++ Lesson1/4-minMaxFromArr/minMaxFromArr/obj/Debug/net8.0/minMaxFromArr.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minMaxFromArr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462faeb1a96c562f1ab3094d5d5ac2fb10e3f7e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c67d99e2e62e0e9e5bd5e471ad15f383a65325")]
 [assembly: System.Reflection.AssemblyProductAttribute("minMaxFromArr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minMaxFromArr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
