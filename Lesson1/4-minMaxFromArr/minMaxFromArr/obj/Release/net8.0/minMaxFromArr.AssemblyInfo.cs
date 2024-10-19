c6796dac89c4799cefa52e70fb1b1e757ab1a63c
mistersavage <michael.um50@yandex.ru>
2024-10-19 14:25:41 +1000

    .

--- Lesson1/4-minMaxFromArr/minMaxFromArr/obj/Release/net8.0/minMaxFromArr.AssemblyInfo.cs
+++ Lesson1/4-minMaxFromArr/minMaxFromArr/obj/Release/net8.0/minMaxFromArr.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("monthsListing")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("minMaxFromArr")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e244c6c831207689fc2ccaece36b88dca592aed")]
-[assembly: System.Reflection.AssemblyProductAttribute("monthsListing")]
-[assembly: System.Reflection.AssemblyTitleAttribute("monthsListing")]
+[assembly: System.Reflection.AssemblyProductAttribute("minMaxFromArr")]
+[assembly: System.Reflection.AssemblyTitleAttribute("minMaxFromArr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
--- Lesson1/5-monthsListing/monthsListing/obj/Release/net8.0/monthsListing.AssemblyInfo.cs
+++ Lesson1/5-monthsListing/monthsListing/obj/Release/net8.0/monthsListing.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("monthsListing")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e244c6c831207689fc2ccaece36b88dca592aed")]
 [assembly: System.Reflection.AssemblyProductAttribute("monthsListing")]
